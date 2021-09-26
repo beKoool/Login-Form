7bdadd791c6c20e5347de13d82f816af30d18f3c
TheCoolDev <thecoolgdev@gmail.com>
2021-09-26 21:01:59 +0545

    Added files

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+  let container = document.querySelector(".main-container");
+  let section = document.querySelector("section");
+  container.classList.toggle("active");
+  section.classList.toggle("active");
+}
