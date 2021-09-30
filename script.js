7fe0fb90197431ecaaafa54e8c7bbf6bcf36ff86
TheCoolDev <thecoolgdev@gmail.com>
2021-09-30 18:26:33 +0545

    Added form

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function clickForm() {
   document.getElementById("h2-signin-up").innerHTML = "RickRoll'd";
   document.getElementById("imgBox").src =
     "https://i.pinimg.com/originals/f7/85/7f/f7857f7dc8194d91da6b825d3ab90fce.gif";
+  section.classList.toggle("changed");
 }
 
 let audio = new Audio("./yes.mp3");
